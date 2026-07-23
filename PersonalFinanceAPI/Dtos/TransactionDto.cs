c7cbd820c060f6de66b708788603af1fdcb5a21e
ibrahim <ibrahim.shaddad@outlook.com>
2026-07-23 17:22:42 +0300

    Created Transaction Dtos

---
+++ PersonalFinanceAPI/Dtos/TransactionDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalFinance.Dtos;
+
+public class TransactionDto
+{
+    public int Id { get; set; }
+    [Required] [MaxLength(100)] public string Title { get; set; } = string.Empty;
+    
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Amount { get; set; }
+    
+    [Required]
+    [MaxLength(20)]
+    public string Type { get; set; } = "Expense";
+
+    [Required]
+    [MaxLength(50)]
+    public string Category { get; set; } = "General";
+
+    public DateTime Date { get; set; } = DateTime.UtcNow;
+}
---
+++ PersonalFinanceAPI/Dtos/TransactionUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalFinance.Dtos;
+
+public class TransactionUpdateDto
+{
+    public string Title { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public string Type { get; set; } = "Expense";
+    public string Category { get; set; } = "General";
+    public DateTime Date { get; set; }
+}
