83eae1d5ce1348afb3b443e7710bab4a1f9d68f2
ibrahim <ibrahim.shaddad@outlook.com>
2026-07-23 17:22:20 +0300

    Created Migration Profile

---
+++ PersonalFinanceAPI/Mapping Profiles/TransactionProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PersonalFinance.Dtos;
+using PersonalFinance.Models;
+
+namespace PersonalFinance.Mapping_Profiles;
+
+public class TransactionProfile : Profile
+{
+    public TransactionProfile()
+    {
+        CreateMap<TransactionDto, Transaction>();
+        CreateMap<TransactionCreateDto, Transaction>().ReverseMap();
+        CreateMap<TransactionUpdateDto, Transaction>();
+
+    }
+    
+}
