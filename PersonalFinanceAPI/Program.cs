322c2aa0bb9457380c76b64a4f980c2d12570514
ibrahim <ibrahim.shaddad@outlook.com>
2026-07-23 17:23:39 +0300

    Removed Initial Weather method

--- PersonalFinanceAPI/Program.cs
+++ PersonalFinanceAPI/Program.cs
@@ -1,11 +1,24 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using PersonalFinance.Data;
+using PersonalFinance.Mapping_Profiles;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+var loggerFactory = LoggerFactory.Create(logging => logging.AddConsole());
+
+var config = new MapperConfiguration(cfg => {
+    cfg.AddProfile<TransactionProfile>();
+}, loggerFactory);
+
+builder.Services.AddSingleton<IMapper>(config.CreateMapper());
+
+builder.Services.AddControllers();
 
 builder.Services.AddCors(options =>
 {
@@ -22,6 +35,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+app.MapControllers();
+
 app.UseCors("AllowAngular");
 
 // Configure the HTTP request pipeline.
@@ -32,24 +47,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
 
 app.Run();
 
